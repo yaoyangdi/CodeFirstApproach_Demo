d810bbc3024a7835e01a56b8a16167e089a6cae9
Yaoyang Ding <ydin0039@student.monash.edu>
2023-08-21 00:08:21 +1000

    Controlled files and cshtml added.

---
+++ WebApplication1/Controllers/UnitsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UnitsController : Controller
+    {
+        private CsharpCBContext db = new CsharpCBContext();
+
+        // GET: Units
+        public ActionResult Index()
+        {
+            return View(db.units.ToList());
+        }
+
+        // GET: Units/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unit unit = db.units.Find(id);
+            if (unit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unit);
+        }
+
+        // GET: Units/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Units/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description")] Unit unit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.units.Add(unit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(unit);
+        }
+
+        // GET: Units/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unit unit = db.units.Find(id);
+            if (unit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unit);
+        }
+
+        // POST: Units/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description")] Unit unit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(unit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(unit);
+        }
+
+        // GET: Units/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unit unit = db.units.Find(id);
+            if (unit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(unit);
+        }
+
+        // POST: Units/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Unit unit = db.units.Find(id);
+            db.units.Remove(unit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
